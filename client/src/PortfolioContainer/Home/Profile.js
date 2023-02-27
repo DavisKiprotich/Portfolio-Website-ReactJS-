d3960180ab730dea15ac22ef22d11cfbcfbe37da
Davis Kiprotich <mylesdavyz@gmail.com>
2023-02-27 21:00:00 +0300

    Structure of the project

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div className='profile-container'>
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='colz'>
+                    <a href="#">
+                        <i className='fa fa-facebook square'></i>
+                    </a>
+                    <a href="#">
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href="#">
+                        <i className='fa fa-twitter'></i>
+                    </a>
+                    <a href="#">
+                        <i className='fa fa-linked'></i>
+                    </a>
+                    <a href="#">
+                        <i className='fa fa-github'></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
