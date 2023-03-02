d254d56f9f4777a3d1e0e5213a8ca8a269dea126
Davis Kiprotich <mylesdavyz@gmail.com>
2023-03-02 09:14:57 +0300

    Styling the profile component

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Typical from 'react-typical'
+import './Profile.css'
 
 export default function 
 () {
@@ -7,22 +8,24 @@ export default function
     <div className='profile-container'>
         <div className='profile-parent'>
             <div className='profile-details'>
-                <div className='colz'>
-                    <a href="#">
-                        <i className='fa fa-facebook square'></i>
-                    </a>
-                    <a href="#">
-                        <i className='fa fa-instagram'></i>
-                    </a>
-                    <a href="#">
-                        <i className='fa fa-twitter'></i>
-                    </a>
-                    <a href="#">
-                        <i className='fa fa-linked'></i>
-                    </a>
-                    <a href="#">
-                        <i className='fa fa-github'></i>
-                    </a>
+                <div className='cols'>
+                    <div className="cols-icon">
+                        <a href="#">
+                            <i className='fa fa-facebook square'></i>
+                        </a>
+                        <a href="#">
+                            <i className='fa fa-instagram square'></i>
+                        </a>
+                        <a href="#">
+                            <i className='fa fa-twitter square'></i>
+                        </a>
+                        <a href="#">
+                            <i className='fa fa-linked '></i>
+                        </a>
+                        <a href="#">
+                            <i className='fa fa-github square'></i>
+                        </a>
+                    </div>
                 </div>
                 <div className='profile-details-name'>
                     <span className='primary-text highlighted-text'>
@@ -50,7 +53,19 @@ export default function
                             />
                         </h1>
                     </span>
+                    <span className="profile-options">
+                        <button className="btn primary-btn">
+                            {" "}
+                            Hire Me {" "}
+                        </button>
+                        <a href="Kiprotich Davis Official.docx" download="Kiprotich Davis Official.docx">
+                            <button className="btn highlighted-btn">My Resume</button>
+                        </a>
+                    </span>
                 </div>
+            </div>
+            <div className="profile-picture">
+                <div className="profile-picture-background"></div>
             </div>
         </div>
     </div>
