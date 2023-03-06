06e843143a84a33426272ac03e7a2bcc088e075f
Davis Kiprotich <mylesdavyz@gmail.com>
2023-03-06 07:56:15 +0300

    Additional folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import Home from './PortfolioContainer/Home/Home';
+import Nav from './PortfolioContainer/Nav/Nav';
+import AboutMe from './PortfolioContainer/AboutMe/About';
+import Experience from './PortfolioContainer/Experience/Experience';
+import Services from './PortfolioContainer/Services/Services';
+import Portfolio from './PortfolioContainer/Portfolio/Portfolio';
+import Testimonial from './PortfolioContainer/Testimonial/Testimonial';
+import Contact from './PortfolioContainer/Contact/Contact';
+import FootNote from './PortfolioContainer/FootNote/FootNote';
 
 
 function App() {
   return (
     <div className="App">
       <Home />
-    </div>
+      <Nav />
+      <AboutMe />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <FootNote />
+  </div>
   );
 }
 
