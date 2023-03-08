d53b6741e2625b486b5a9595e1d9c87551d87c95
Davis Kiprotich <mylesdavyz@gmail.com>
2023-03-08 21:35:23 +0300

    Client2 created

---
+++ client2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './ProfileContainer/Home/Home'
+import Nav from './ProfileContainer/Nav/Nav'
+
+function App() {
+    return (
+      <div className="App">
+        <Home />
+        <Nav />
+    </div>
+    );
+  }
+  
+  export default App;
+  
---
+++ client2/src/ProfileContainer/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css'
+import {AiOutlineHome} from 'react-icons/ai' 
+
+const Nav = () => {
+  return (
+    <div>
+        <a href="#"><AiOutlineHome/></a>
+        <a href="#"><AiOutlineHome/></a>
+        <a href="#"><AiOutlineHome/></a>
+        <a href="#"><AiOutlineHome/></a>
+        <a href="#"><AiOutlineHome/></a>
+    </div>
+  )
+}
+
+export default Nav
