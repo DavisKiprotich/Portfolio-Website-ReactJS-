0ad043362d2bd497373970235699831421c01ade
Davis Kiprotich <mylesdavyz@gmail.com>
2023-03-09 08:39:29 +0300

    Modify the Nav

--- client2/src/App.js
+++ client2/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './ProfileContainer/Home/Home'
 import Nav from './ProfileContainer/Nav/Nav'
+import About from './ProfileContainer/AboutMe/About'
 
 function App() {
     return (
       <div className="App">
         <Home />
         <Nav />
+        <About />
     </div>
     );
   }
--- client2/src/ProfileContainer/Nav/Nav.js
+++ client2/src/ProfileContainer/Nav/Nav.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Nav.css'
 import {AiOutlineHome} from 'react-icons/ai' 
+import {FaUserTie} from 'react-icons/fa' 
+import {RiServiceLine} from 'react-icons/ri' 
+import {AiFillBook} from 'react-icons/ai'
+import {MdOutlineMessage} from 'react-icons/md'
+import { useState } from 'react'
 
 const Nav = () => {
   return (
-    <div>
-        <a href="#"><AiOutlineHome/></a>
-        <a href="#"><AiOutlineHome/></a>
-        <a href="#"><AiOutlineHome/></a>
-        <a href="#"><AiOutlineHome/></a>
-        <a href="#"><AiOutlineHome/></a>
-    </div>
+    <nav>
+        <a href="#home"><AiOutlineHome/></a>
+        <a href="#about"><FaUserTie/></a>
+        <a href="#experience"><AiFillBook/></a>
+        <a href="#services"><RiServiceLine/></a>
+        <a href="#contact"><MdOutlineMessage/></a>
+    </nav>
   )
 }
 
