a581669b4e1d5ff6c3326954c6e1f1a0bd02227b
Davis Kiprotich <mylesdavyz@gmail.com>
2023-03-15 07:46:31 +0300

    Created the services file

--- client2/src/ProfileContainer/Services/Services.js
+++ client2/src/ProfileContainer/Services/Services.js
@@ -1,9 +1,86 @@
 import React from 'react'
 import './Services.css'
-
+import {GiCheckMark} from 'react-icons/gi'
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h2>Services</h2>
+      <div className='services-container'>
+        <article class='service'>
+          <div className='service-head'>
+            <h3>Front-end Web Design</h3>
+          </div>
+          <ul className='service-list'>
+            <li>
+              <GiCheckMark />
+              <p>Highly customizable web app with interactive and complex User Interfaces.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Create a dynamic, single-page web application</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Build an easily scalable feature-rich web app that can grow up to an enterprise level.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Ensure responsive design and high performance across different devices and operating systems</p>
+            </li>
+          </ul>
+        </article>
+        <article class='service'>
+          <div className='service-head'>
+            <h3>Cloud Engineering services</h3>
+          </div>
+          <ul className='service-list'>
+            <li>
+              <GiCheckMark />
+              <p>Migrate and modernize applications</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Organize and optimize infrastructure and manage cost efficiently.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Maintaining the cloud infrastructure</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Nanage and support network infrastructures and connections between clients</p>
+            </li>
+          </ul>
+        </article>
+        <article class='service'>
+          <div className='service-head'>
+            <h3>Electrical Engineering</h3>
+          </div>
+          <ul className='service-list'>
+            <li>
+              <GiCheckMark />
+              <p>Installation of new electrical machines</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Estimation and budeting of electrical machines</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Design of single line electrical diagrams</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Electrical checkout and start up</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>System trouble shooting</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
