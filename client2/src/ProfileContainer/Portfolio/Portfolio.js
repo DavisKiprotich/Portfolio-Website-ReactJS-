622132b3a65289d5fe12a14b04e3a930c0e52533
Davis Kiprotich <mylesdavyz@gmail.com>
2023-03-20 07:55:32 +0300

    Styling portfolio js

--- client2/src/ProfileContainer/Portfolio/Portfolio.js
+++ client2/src/ProfileContainer/Portfolio/Portfolio.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../Assets/portfolio3.jpg'
+import IMG2 from '../../Assets/portfolio1.jpg'
+import IMG3 from '../../Assets/portfolio2.jpg'
+
 
 const Portfolio = () => {
     return (
       <section id='portfolio'>
-        <h5>My Recent Work</h5>
         <h2>Portfolio</h2>
         <div className='portfolio-container'>
             <article className='portfolio-item'>
-                <div className='portfolio-item-image'></div>
+                <div className='portfolio-item-image'>
+                    <img src={IMG1} alt="" />
+                </div>
                 <h3>Responsive E-commerce website</h3>
-                <a href=''>Github</a>
+                <div className='portfolio-item-tag'>
+                    <a href='https://github.com/' className='btn' target='_blank'>Github</a>
+                </div>
+            </article>
+            <article className='portfolio-item'>
+                <div className='portfolio-item-image'>
+                    <img src={IMG2} alt="" />
+                </div>
+                <h3>Responsive E-commerce website</h3>
+                <div className='portfolio-item-tag'>
+                    <a href='https://github.com/' className='btn' target='_blank'>Github</a>
+                </div>
+            </article>
+            <article className='portfolio-item'>
+                <div className='portfolio-item-image'>
+                    <img src={IMG3} alt="" />
+                </div>
+                <h3>Responsive E-commerce website</h3>
+                <div className='portfolio-item-tag'>
+                    <a href='https://github.com/' className='btn' target='_blank'>Github</a>
+                </div>
             </article>
         </div>
       </section>
