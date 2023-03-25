0c47bc483773daf5e2467878c93a04bcfbd3c114
Davis Kiprotich <mylesdavyz@gmail.com>
2023-03-25 21:32:07 +0300

    Created contact file

--- client2/src/ProfileContainer/Contact/Contact.js
+++ client2/src/ProfileContainer/Contact/Contact.js
@@ -1,9 +1,42 @@
 import React from 'react'
 import './Contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { AiOutlineWhatsApp } from 'react-icons/ai'
 
 const Contact = () => {
   return (
-    <section id='contact'>Portfolio</section>
+    <section id='contact'>
+      <h2>Contact Me</h2>
+      <div className='contact-container'>
+        <div className='contact-options'>
+          <article className='contact-option'>
+          <MdOutlineEmail />
+          <h4>Email</h4>
+          <h5>davyzkorir@gmail.com</h5>
+          <a href='mailto:davyzkorir@gmail.com' target='_blank'>Send a Message</a>
+          </article>
+          <article className='contact-option'>
+          <RiMessengerLine />
+          <h4>Messenger</h4>
+          <h5>Miles Best</h5>
+          <a href='https://m.me/' target='_blank'>Send a Message</a>
+          </article>
+          <article className='contact-option'>
+          <AiOutlineWhatsApp />
+          <h4>Whatsapp</h4>
+          <h5>Humble Beast</h5>
+          <a href='https://api.whatsapp.com/send?phone=+254705013074' target='_blank'>Send your message</a>
+          </article>
+        </div>
+        <form action=''>
+          <input type='text' name='name' placeholder='Your Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary '>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
