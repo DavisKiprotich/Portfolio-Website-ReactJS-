f291c91f2271b4fd94ebd011ba856b2f4e0a7a7f
Kiprotich Davis <mylesdavyz@gmail.com>
2024-01-23 15:35:39 +0300

    Template images created

--- client2/src/ProfileContainer/AboutMe/About.js
+++ client2/src/ProfileContainer/AboutMe/About.js
@@ -20,17 +20,17 @@ const About = () => {
               <article className='about-card'>
                 <MdOutlineWorkHistory className='about-icon'/>
                 <h5>Experience</h5>
-                <small> -1 year Working</small>
+                <small> +1 year Working</small>
                 </article>
                 <article className='about-card'>
                   <GiSwordsPower className='about-icon'/>
                   <h5>Skills</h5>
-                  <small>4+ Gained</small>
+                  <small>8+ Gained</small>
                 </article>
               <article className='about-card'>
                 <GiOpenFolder className='about-icon'/>
                 <h5>Projects</h5>
-                <small> 3+ Completed</small>
+                <small> 8+ Completed</small>
               </article>
             </div>
             <p>I am a seasoned Front End Developer with over 2 years of expertise in crafting visually appealing and user-friendly web applications. Proficient in frameworks like React and Next.js, I seamlessly integrate with backend technologies such as Django and Node.js. My skills extend to HTML, CSS, JavaScript and Wordpress ensuring clean, efficient, and well-documented code. With a keen eye for detail and a commitment to high-quality work, I actively contribute to cross-functional teams, bringing innovative solutions to complex problems. My dynamic skill set extends to website development, emphasizing adaptability, effective communication, and continuous improvement within the Engineering team. </p>
